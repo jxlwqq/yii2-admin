8fb7987c5416901ecd8a71a682ba7e80a111c2a5
ye.yin <ye.yin@24tidy.com>
2016-08-06 12:29:37 +0800

    change view theme

--- config/web.php
+++ config/web.php
@@ -46,6 +46,13 @@ $config = [
             ],
         ],
         */
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                ],
+            ],
+        ],
     ],
     'params' => $params,
 ];
